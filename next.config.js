3c8c8345bbee9531b1db761c578b7abc2d6cca23
Hariharan Selvaraj <hariharan.2309.s@icloud.com>
2025-03-04 16:17:16 +0100

    update next files

--- next.config.js
+++ next.config.js
@@ -4,15 +4,7 @@ const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["plant-doctor-hxptp783pkkz.deno.dev"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "plant-doctor-hxptp783pkkz.deno.dev",
-        port: "",
-        pathname: "/**",
-      },
-    ],
+    unoptimized: true, // Ensure images in public/ load properly
   },
   env: {
     PLANT_DOCTER_LOCAL_API_BASE_URL: "http://localhost:8000",
