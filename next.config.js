b47e4fc661f8ed035633b0f064c797f3452cf916
Hariharan Selvaraj <hariharan.2309.s@icloud.com>
2025-03-04 17:17:37 +0100

    some more changes to fix the icons

--- next.config.js
+++ next.config.js
@@ -17,8 +17,7 @@ const nextConfig = {
     ],
     unoptimized: true, // Ensure images in public/ load properly
   },
-  basePath: isProd ? "/plant-doctor-hxptp783pkkz.deno.dev" : "",
-  assetPrefix: isProd ? "/plant-doctor-hxptp783pkkz.deno.dev/" : "",
+  trailingSlash: true, // Ensures static files are accessible
   env: {
     PLANT_DOCTER_LOCAL_API_BASE_URL: "http://localhost:8000",
     PLANT_DOCTER_API_BASE_URL: "https://plant-doctor-hxptp783pkkz.deno.dev"
