232e241f4198bbd2e604bdfea4f7a3a1deb92077
Hariharan Selvaraj <hariharan.2309.s@icloud.com>
2025-03-04 14:35:10 +0100

    update config

--- next.config.js
+++ next.config.js
@@ -20,7 +20,7 @@ const nextConfig = {
   },
   webpack(config) {
     // Adding alias for '@' to point to 'src' directory
-    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
     return config;
   }
 };
