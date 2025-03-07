d1728a07f765a1ad5968461bc804ec200e9fb20a
Hariharan Selvaraj <hariharan.2309.s@icloud.com>
2025-03-07 17:56:35 +0100

    url changes

--- next.config.js
+++ next.config.js
@@ -4,11 +4,11 @@ const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["plant-doctor-fyq6g8m5npzs.deno.dev"],
+    domains: ["plant-doctor-76hdm8jnmyar.deno.dev"],
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "plant-doctor-fyq6g8m5npzs.deno.dev",
+        hostname: "plant-doctor-76hdm8jnmyar.deno.dev",
         port: "",
         pathname: "/**",
       },
@@ -18,7 +18,7 @@ const nextConfig = {
   output: 'export', // Ensures Next.js generates a static build
   env: {
     PLANT_DOCTER_LOCAL_API_BASE_URL: "http://localhost:8000",
-    PLANT_DOCTER_API_BASE_URL: "https://plant-doctor-fyq6g8m5npzs.deno.dev"
+    PLANT_DOCTER_API_BASE_URL: "https://plant-doctor-76hdm8jnmyar.deno.dev"
   },
   webpack(config) {
     // Adding alias for '@' to point to 'src' directory
--- src/services/api.ts
+++ src/services/api.ts
@@ -5,10 +5,10 @@ import {
 } from "@/types/api";
 import { createClient } from '@supabase/supabase-js';
 
-const API_BASE_URL = process.env.PLANT_DOCTER_API_BASE_URL || "https://plant-doctor-fyq6g8m5npzs.deno.dev";
+const API_BASE_URL = process.env.PLANT_DOCTER_API_BASE_URL || "https://plant-doctor-76hdm8jnmyar.deno.dev";
 //const API_BASE_URL = "http://localhost:8000";
-const PLANT_DOCTER_SUPABASE_URL = process.env.PLANT_DOCTER_SUPABASE_URL || "https://tztqrwfrxgdnghfvdluj.supabase.co";
-const PLANT_DOCTER_SUPABASE_SERVICE_ROLE_KEY = process.env.PLANT_DOCTER_SUPABASE_SERVICE_ROLE_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6dHFyd2ZyeGdkbmdoZnZkbHVqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MTI3MDM4MSwiZXhwIjoyMDU2ODQ2MzgxfQ.p8JNmOc72V437eqbsVp5WGanll3PfvRXBEZQR2cwvBw";
+const PLANT_DOCTER_SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://tztqrwfrxgdnghfvdluj.supabase.co";
+const PLANT_DOCTER_SUPABASE_SERVICE_ROLE_KEY = process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6dHFyd2ZyeGdkbmdoZnZkbHVqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MTI3MDM4MSwiZXhwIjoyMDU2ODQ2MzgxfQ.p8JNmOc72V437eqbsVp5WGanll3PfvRXBEZQR2cwvBw";
 const supabaseClient = createClient(PLANT_DOCTER_SUPABASE_URL, PLANT_DOCTER_SUPABASE_SERVICE_ROLE_KEY);
 
 export const uploadImageAndPredict = async (
