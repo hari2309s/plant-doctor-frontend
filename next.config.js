e1e101c8c10d651e5eb6cf674e114edcc73e1bd6
Hariharan Selvaraj <hariharan.2309.s@icloud.com>
2025-03-07 13:31:39 +0100

    changes

--- next.config.js
+++ next.config.js
@@ -4,11 +4,11 @@ const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["plant-doctor-z34z08tzna3c.deno.dev"],
+    domains: ["plant-doctor-2neqgyy367t4.deno.dev"],
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "plant-doctor-z34z08tzna3c.deno.dev",
+        hostname: "plant-doctor-2neqgyy367t4.deno.dev",
         port: "",
         pathname: "/**",
       },
@@ -18,7 +18,7 @@ const nextConfig = {
   output: 'export', // Ensures Next.js generates a static build
   env: {
     PLANT_DOCTER_LOCAL_API_BASE_URL: "http://localhost:8000",
-    PLANT_DOCTER_API_BASE_URL: "https://plant-doctor-z34z08tzna3c.deno.dev"
+    PLANT_DOCTER_API_BASE_URL: "https://plant-doctor-2neqgyy367t4.deno.dev"
   },
   webpack(config) {
     // Adding alias for '@' to point to 'src' directory
